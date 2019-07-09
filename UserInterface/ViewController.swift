47ea9447d315e363875270bd510c7d4a754cdfc6
Will <fgnickson@gmail.com>
2019-07-09 11:07:44 +0100

    Initial Commit

---
+++ UserInterface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserInterface
+//
+//  Created by Will Nickson on 09/07/2019.
+//  Copyright © 2019 find-my-face. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
